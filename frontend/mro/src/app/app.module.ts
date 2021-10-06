7ad6cc3fba4b8e5958d1439e91c4ad75b447faf5
Tom Han <tomhyhan@gmail.com>
2021-10-05 19:01:32 -0700

    header && footer

--- frontend/mro/src/app/app.module.ts
+++ frontend/mro/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { SparePartComponent } from './components/master/spare-part/spare-part.component';
 import { SparePartsComponent } from './components/master/spare-parts/spare-parts.component';
 import { AddSparePartComponent } from './components/master/add-spare-part/add-spare-part.component';
 import { EditSparePartComponent } from './components/master/edit-spare-part/edit-spare-part.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -17,8 +20,16 @@ import { EditSparePartComponent } from './components/master/edit-spare-part/edit
     SparePartsComponent,
     AddSparePartComponent,
     EditSparePartComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })

cd4ae088e7d6e557c871f191f44dd0175ac3f734
tama <rpi.irman@gmail.com>
2021-10-11 17:06:17 -0700

    attempt at aircraft fix

---
+++ frontend/mro/src/app/components/master/aircraft/aircrafts/aircrafts.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { aircraftService } from 'src/app/services/master/aircraft/aircraft.service';
+
+@Component({
+  selector: 'app-aircraft',
+  templateUrl: './aircrafts.component.html',
+  styleUrls: ['./aircrafts.component.css']
+})
+export class aircraftsComponent implements OnInit {
+  aircraft: any = [];
+
+  displayedColumns: string[] = [
+    'aircraft_name',
+    'remarks',
+  ];
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
+  constructor(private aircraftService: aircraftService) { }
+
+
+  ngAfterViewInit(): void {
+    this.aircraftService.getaircraft().subscribe(aircraft => this.aircraft = aircraft);
+    this.aircraft.paginator = this.paginator;
+  }
+
+  ngOnInit(): void {
+    this.aircraftService.getaircraft().subscribe((aircraft) => {
+      console.log(aircraft);
+      this.aircraft = aircraft;
+    });
+  }
+  createTask(aircraft: any) {
+    this.aircraftService
+      .addaircraft(aircraft)
+      .subscribe((aircraft: any) => {
+        this.aircraft = [...this.aircraft, aircraft[0]];
+      });
+  }
+
+  updateaircraft(aircraft: any) {
+    this.aircraftService
+      .updateaircraft(aircraft.aircraft, aircraft.id)
+      .subscribe((updated: any) => {
+        console.log(updated)
+        const newaircrafts = this.aircraft.map((aircraft: any) => {
+          if (aircraft.material_aircraft_id === updated[0].material_aircraft_id) {
+            return updated[0];
+          }
+          return aircraft;
+        });
+        this.aircraft = newaircrafts;
+      });
+  }
+
+}

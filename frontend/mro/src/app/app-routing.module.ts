266fd3e87a09ca45f9c7666fb0330fe7579138ef
Inggy <71790092+zengingrid@users.noreply.github.com>
2021-10-12 19:11:52 -0700

    merge with development

--- frontend/mro/src/app/app-routing.module.ts
+++ frontend/mro/src/app/app-routing.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-import { EmployeesComponent } from './components/master/employees-main/employees/employees.component';
-
-
-const routes: Routes = [
-  { path: 'master/employees', component: EmployeesComponent },
-=======
 
 import { SparePartsComponent } from './components/master/sparePart/spare-parts/spare-parts.component';
 import { ProjectsComponent } from './components/master/project/projects/projects.component';
 import { PurchaseOrderComponent } from './components/purchase-order/purchase-order.component';
 import { PurchaseOrderCreateComponent } from './components/purchase-order-create/purchase-order-create.component';
-import { EmployeesComponent } from './components/master/employees-main/employees/employees.component';
+import { EmployeesComponent } from './components/master/employees/employees/employees.component';
 import { UomPartsComponent } from './components/master/uom/uom-parts/uom-parts.component';
 import { VendorsComponent } from './components/master/vendor/vendors/vendors.component';
 import { aircraftsComponent } from './components/master/aircraft/aircrafts/aircrafts.component'
@@ -27,7 +20,6 @@ const routes: Routes = [
   { path: 'master/uom', component: UomPartsComponent },
   { path: 'master/vendor', component:VendorsComponent},
   { path: 'master/aircraft', component:aircraftsComponent},
->>>>>>> e05ff3ec5eac9ec76e9344a81f065e7883a75261
 ];
 
 @NgModule({
@@ -36,7 +28,4 @@ const routes: Routes = [
 })
 export class AppRoutingModule {}
 
-<<<<<<< HEAD
-=======
 //
->>>>>>> e05ff3ec5eac9ec76e9344a81f065e7883a75261
--- frontend/mro/src/app/app.module.ts
+++ frontend/mro/src/app/app.module.ts
@@ -31,9 +31,9 @@ import { AddProjectComponent } from './components/master/project/add-project/add
 import { EditProjectComponent } from './components/master/project/edit-project/edit-project.component';
 import { ProjectsComponent } from './components/master/project/projects/projects.component';
 
-import { EmployeesComponent } from './components/master/employees-main/employees/employees.component';
-import { AddEmployeeComponent } from './components/master/employees-main/add-employee/add-employee.component';
-import { EditEmployeeComponent } from './components/master/employees-main/edit-employee/edit-employee.component';
+import { EmployeesComponent } from './components/master/employees/employees/employees.component';
+import { AddEmployeeComponent } from './components/master/employees/add-employee/add-employee.component';
+import { EditEmployeeComponent } from './components/master/employees/edit-employee/edit-employee.component';
 
 import { UomPartsComponent } from './components/master/uom/uom-parts/uom-parts.component';
 import { AddUomComponent } from './components/master/uom/add-uom/add-uom-part.component';
